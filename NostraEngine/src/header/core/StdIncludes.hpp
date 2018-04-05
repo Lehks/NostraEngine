fe95c5c34997e95a7bd716ddddf5146b5b680837
Lukas Reichmann <lukas@reichmann-info.de>
2018-04-05 19:37:58 +0200

    Implemented first, rudimentary version of the file StdIncludes.hpp

---
+++ NostraEngine/src/header/core/StdIncludes.hpp
@@ -0,0 +1,18 @@
+#ifndef NOE_CORE_STD_INCLUDES_HPP
+#define NOE_CORE_STD_INCLUDES_HPP
+
+/**
+\file core/StdIncludes.hpp
+
+\author  Lukas Reichmann
+\version 0.0.1
+\since   0.0.1
+
+\brief A file that is usually included by all other files of the Nostra Engine.
+*/
+
+#ifndef NOE
+#define NOE nostra::engine
+#endif
+
+#endif
