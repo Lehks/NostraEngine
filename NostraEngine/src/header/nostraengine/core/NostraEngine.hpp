7b3cdcc6c945514384c0996e8415ad2a5030705b
Dennis Franz <dennisfranz96@gmx.net>
2018-04-09 21:22:04 +0200

    Created the main loop of the engine.

---
+++ NostraEngine/src/header/nostraengine/core/NostraEngine.hpp
@@ -0,0 +1,68 @@
+#ifndef NOSTRA_ENGINE_HPP
+#define NOSTRA_ENGINE_HPP
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "nostraengine/core/StdIncludes.hpp"
+#include "nostrautils/core/StdIncludes.hpp"
+#include <iostream>
+
+/**
+\file core/NostraEngine
+
+\author	 Dennis Franz
+\author	 Leslie Marxen
+\version 1.0.0
+\since   1.0.0
+
+\brief This is the main file of this engine. It contains the 3 main methods: initialize method , start method and terminate method.
+
+\details 
+Initialize-Method:	Every other "main" file , class or module needs to be initialized before used which can be done in the initialize method.
+Start-Method:		In the start Method is the main loop of the engine, if there is something in the render Method it will be updated here.
+Terminate-Method:	If the window gets closed , the terminate method will clean up and "terminate" every module which got initialized.
+*/
+namespace NOE
+{
+	class NostraEngine
+	{
+	private:
+		/**
+		\brief an ID for the Engine.
+		*/
+		NOU::int32 ID;
+		/**
+		\return		void
+
+		\brief The Render-Method. Everything put in here will be, after each iteration of the loop in the start method, updated.
+		*/
+		void render();
+	public:
+		/**
+		\brief Standard constructor for now.
+		*/
+		NostraEngine(NOU::int32 ID);
+		/**
+		/return		NOU::int8
+
+		\brief Initialization-Method. Everything put in here will be initialized for later usage.
+		*/
+		NOU::int8 init();
+		/**
+		/return		NOU::int8
+
+		\brief Start-Method. Holds the main loop of the engine. 
+		*/
+		NOU::int8 start();
+		/**
+		/return		NOU::int8
+
+		\brief Terminate-Method. Everything put in here will be closed the right way if the program will be terminated.
+		*/
+		NOU::int8 terminate();
+	};
+}
+
+#endif
+
---
+++ NostraEngine/src/source/nostraengine/core/NostraEngine.cpp
@@ -0,0 +1,65 @@
+#include "nostraengine/core/NostraEngine.hpp"
+
+void NOE::NostraEngine::render()
+{
+	//------------------------------------------------------------
+	//
+	//PUT YOUR RENDER STUFF HERE !
+	//
+}
+
+NOE::NostraEngine::NostraEngine(NOU::int32 ID) :
+	ID(ID)
+{}
+
+NOU::int8 NOE::NostraEngine::init()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//The two lines above mean OpenGL version 4.0.
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+	//------------------------------------------------------------
+	//
+	//DO YOUR STANDARD INIT METHODE'S HERE !
+	//
+
+
+	return 0;
+}
+
+NOU::int8 NOE::NostraEngine::start()
+{
+
+	init();
+	if (init != 0)
+	{
+		std::cout << "An error occurred during initialization."  << std::endl;
+		return 1;
+	}
+
+	while (true)			//DON'T RUN IT !!!!
+	{
+		render();
+	}
+
+	terminate();
+
+	return 0;
+}
+
+NOU::int8 NOE::NostraEngine::terminate()
+{
+	glfwTerminate();
+
+	//------------------------------------------------------------
+	//
+	//DO YOUR STANDARD TERMINATE METHODE'S HERE !
+	//
+	return 0;
+}
