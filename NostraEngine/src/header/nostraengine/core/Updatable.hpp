49c30b3a61277460c375b4158159276927677567
Leslie <leslie@bmarxen.de>
2018-04-19 14:34:31 +0200

    Added some Doc for Updatable.hpp

--- NostraEngine/src/header/nostraengine/core/Updatable.hpp
+++ NostraEngine/src/header/nostraengine/core/Updatable.hpp
@@ -4,13 +4,32 @@
 #include "nostrautils/NostraUtils.hpp"
 #include "nostrautils/StdIncludes.hpp"
 
+/**
+\author  Leslie Marxen
+\since   0.1.0
+\version 0.1.0
+\brief  This file provides an abstract class that provides some functionallity
+        in terms of Updatabillity for later Objects 
+ */
+
+
+
 namespace NOE
 {
 
     class NOU_CLASS Updatable
     {
         public:
+            
+            /**
+            \brief Abstract function that defines behaivour that needs to be executed before every render frame
+            */
             virtual void earlyUpdate();
+            /**
+            \brief Abstract function that defines some functionallity the implemented object does every Render iteration
+            \detail For example if a player wants to move the ingame Player object, the object has to scan for input every
+                    frame
+            */
             virtual void update() = 0;
             virtual void lateUpdate();
     };
