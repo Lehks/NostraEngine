ac099bc695366043d4048a32c62bf23505407340
Leslie <leslie@bmarxen.de>
2018-04-25 08:52:45 +0200

    removed some more artifacts and fixed doc

---
+++ NostraEngine/src/header/nostraengine/material_system/GLSLCompiler.hpp
@@ -0,0 +1,17 @@
+#ifndef NOE_MATERIAL_SYSTEM_GLSL_COMPILER_HPP
+#define NOE_MATERIAL_SYSTEM_GLSL_COMPILER_HPP
+
+/**
+\file material_system/GLSLCompiler.hpp
+
+\author  Leslie Marxen
+\version 0.0.1
+\since   0.0.1
+
+\brief  A file that provides the implementation of the GLSL Shader used by the Nostra Engine for rendering with OpenGL.
+*/
+
+
+
+
+#ifndef
--- NostraEngine/src/header/nostraengine/material_system/ShaderCompiler.hpp
+++ NostraEngine/src/header/nostraengine/material_system/ShaderCompiler.hpp
@@ -1,6 +1,8 @@
 #ifndef NOE_CORE_MATERIAL_SYSTEM_SHADER_COMPILER_HPP
 #define NOE_CORE_MATERIAL_SYSTEM_SHADER_COMPILER_HPP
 
+#include "nostraengine/core/StdIncludes.hpp"
+
 /**
 \file material_system/ShaderCompiler.hpp
 
@@ -8,22 +10,29 @@
 \version 0.0.1
 \since   0.0.1
 
-\brief  A file that provides the base of any compiler class, every other Compiler implementation has to inherit from this interface.
-        Usually a defaiult Shader provided by this Engine will be made fitting to the current context and compiled for later usage 
-        on the GPU.
+\brief  A file that provides the base of any compiler class.
 */
 
-class ShaderCompiler
+/**
+\brief  This is an interface. Every other Compiler implementation has to inherit from this interface.
+        Usually a defaiult Shader provided by this Engine will be made fitting to the current context and compiled for later usage 
+        on the GPU. The compiler should perform the tasks of Generating the source Shader (readable by the used GPU interface) and 
+        compiling those for a later use on the GPU
+*/
+namespace NOE::NOE_MATSYS
 {
-public:
-    /**
-    \brief a method that compiles the generated source shader.
-    */
-    virtual void compile()  = 0;
-    /**
-    \brief a method that generates the source shader based on the used material and the default shader
-    */
-    virtual void generate() = 0;
-};
 
+    class ShaderCompiler
+    {
+    public:
+        /**
+        \brief a method that compiles the generated source shader.
+        */
+        virtual void compile()  = 0;
+        /**
+        \brief a method that generates the source shader based on the used material and the default shader
+        */
+        virtual void generate() = 0;
+    };
+}
 #endif
