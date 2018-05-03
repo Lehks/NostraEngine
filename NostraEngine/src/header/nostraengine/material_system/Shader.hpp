627d925e5ee8c7c34eed8aff868cc0528ee40bb7
NooCi <Dennis2010>
2018-05-03 13:34:35 +0200

    further implemented Shaderclass

--- NostraEngine/src/header/nostraengine/material_system/Shader.hpp
+++ NostraEngine/src/header/nostraengine/material_system/Shader.hpp
@@ -16,19 +16,31 @@ namespace NOE::NOE_MATSYS
     class NOU_CLASS Shader
     {
     private:
-        NOU::NOU_DAT_ALG::String8 vertexShaderPath;
-        NOU::NOU_DAT_ALG::String8 fragmentShaderPath;
+        const NOU::NOU_DAT_ALG::StringView8 *vertexShaderSource;
+        const NOU::NOU_DAT_ALG::StringView8 *fragmentShaderSource;
+        const NOU::NOU_DAT_ALG::StringView8 *geometryShaderSource;
+
+        void link(NOU::uint32 vertex, NOU::uint32 fragment, NOU::uint32 geometry = NULL);
     public:
         NOU::uint32 ID;
 
-        Shader(NOU::NOU_DAT_ALG::String8 vertexShaderPath, NOU::NOU_DAT_ALG::String8 fragmentShaderPath);
+        Shader(NOU::int32 ID, const NOU::NOU_DAT_ALG::StringView8 *vertexShaderPath, const NOU::NOU_DAT_ALG::StringView8 *fragmentShaderPath, const NOU::NOU_DAT_ALG::StringView8 *geometryShaderSource = nullptr);
 
         void use();
-        void link();
         void compile();
-        void setBool();
-        void setInt();
-        void setFloat();
+        void setBool(NOU::NOU_DAT_ALG::StringView8 name, NOU::boolean value) const;
+        void setInt(NOU::NOU_DAT_ALG::StringView8 name, NOU::int32 value) const;
+        void setFloat(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 value) const;
+        void setVec2(NOU::NOU_DAT_ALG::StringView8 name, const glm::vec2 &value) const;
+        void setVec3(NOU::NOU_DAT_ALG::StringView8 name, const glm::vec3 &value) const;
+        void setVec4(NOU::NOU_DAT_ALG::StringView8 name, const glm::vec4 &value) const;
+        void setVec2(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 x, NOU::float32 y) const;
+        void setVec3(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 x, NOU::float32 y, NOU::float32 z) const;
+        void setVec4(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 x, NOU::float32 y, NOU::float32 z, NOU::float32 w) const;
+        void setMat2(NOU::NOU_DAT_ALG::StringView8 name, const glm::mat2 &mat) const;
+        void setMat3(NOU::NOU_DAT_ALG::StringView8 name, const glm::mat3 &mat) const;
+        void setMat4(NOU::NOU_DAT_ALG::StringView8 name, const glm::mat4 &mat) const;
+        void checkCompileErrors(GLuint shader, std::string type);
     };
 }
 
--- NostraEngine/src/source/nostraengine/material_system/Shader.cpp
+++ NostraEngine/src/source/nostraengine/material_system/Shader.cpp
@@ -6,27 +6,145 @@
 
 namespace NOE::NOE_MATSYS
 {
-    Shader::Shader(NOU::NOU_DAT_ALG::String8 vertexShaderPath, NOU::NOU_DAT_ALG::String8 fragmentShaderPath) :
-        vertexShaderPath(vertexShaderPath),
-        fragmentShaderPath(fragmentShaderPath)
+    Shader::Shader(NOU::int32 ID, const NOU::NOU_DAT_ALG::StringView8 *vertexShaderSource, const NOU::NOU_DAT_ALG::StringView8 *fragmentShaderSource, const NOU::NOU_DAT_ALG::StringView8 *geometryShaderSource) :
+        vertexShaderSource(vertexShaderSource),
+        fragmentShaderSource(fragmentShaderSource),
+        geometryShaderSource(geometryShaderSource)
     {}
 
     void Shader::use()
-    {}
+    {
+        glUseProgram(ID);
+    }
 
-    void Shader::link()
-    {}
+    void Shader::link(NOU::uint32 vertex, NOU::uint32 fragment, NOU::uint32 geometry)
+    {
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        if(geometryShaderSource != nullptr)
+            glAttachShader(ID, geometry);
+        glLinkProgram(ID);
+        checkCompileErrors(ID, "PROGRAM");
+        // delete the shaders as they're linked into our program now and no longer necessery
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+        if(geometryShaderSource != nullptr)
+            glDeleteShader(geometry);
+    }
 
     void Shader::compile()
-    {}
+    {
+        NOU::uint32 vertex, fragment;
+        NOU::int32 success;
+        NOU::char8 infoLog[512];
+        // vertex shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        const char * vShaderSource = vertexShaderSource->rawStr();
+        glShaderSource(vertex, 1, &vShaderSource, NULL);
+        glCompileShader(vertex);
+        checkCompileErrors(vertex, "VERTEX");
+        // fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        const char * fShaderCode = fragmentShaderSource->rawStr();
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+        // if geometry shader is given, compile geometry shader
+        unsigned int geometry;
+        if(geometryShaderSource != nullptr)
+        {
+            const char * gShaderCode = geometryShaderSource->rawStr();
+            geometry = glCreateShader(GL_GEOMETRY_SHADER);
+            glShaderSource(geometry, 1, &gShaderCode, NULL);
+            glCompileShader(geometry);
+            checkCompileErrors(geometry, "GEOMETRY");
+        }
 
-    void Shader::setBool()
-    {}
+        link(vertex, fragment, geometry);
+    }
 
-    void Shader::setInt()
-    {}
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void Shader::setBool(NOU::NOU_DAT_ALG::StringView8 name, NOU::boolean value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.rawStr()), (int)value);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setInt(NOU::NOU_DAT_ALG::StringView8 name, NOU::int32 value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.rawStr()), value);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setFloat(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.rawStr()), value);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setVec2(NOU::NOU_DAT_ALG::StringView8 name, const glm::vec2 &value) const
+    {
+        glUniform2fv(glGetUniformLocation(ID, name.rawStr()), 1, &value[0]);
+    }
+    void Shader::setVec2(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 x, NOU::float32 y) const
+    {
+        glUniform2f(glGetUniformLocation(ID, name.rawStr()), x, y);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setVec3(NOU::NOU_DAT_ALG::StringView8 name, const glm::vec3 &value) const
+    {
+        glUniform3fv(glGetUniformLocation(ID, name.rawStr()), 1, &value[0]);
+    }
+    void Shader::setVec3(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 x, NOU::float32 y, NOU::float32 z) const
+    {
+        glUniform3f(glGetUniformLocation(ID, name.rawStr()), x, y, z);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setVec4(NOU::NOU_DAT_ALG::StringView8 name, const glm::vec4 &value) const
+    {
+        glUniform4fv(glGetUniformLocation(ID, name.rawStr()), 1, &value[0]);
+    }
+    void Shader::setVec4(NOU::NOU_DAT_ALG::StringView8 name, NOU::float32 x, NOU::float32 y, NOU::float32 z, NOU::float32 w) const
+    {
+        glUniform4f(glGetUniformLocation(ID, name.rawStr()), x, y, z, w);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setMat2(NOU::NOU_DAT_ALG::StringView8 name, const glm::mat2 &mat) const
+    {
+        glUniformMatrix2fv(glGetUniformLocation(ID, name.rawStr()), 1, GL_FALSE, &mat[0][0]);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setMat3(NOU::NOU_DAT_ALG::StringView8 name, const glm::mat3 &mat) const
+    {
+        glUniformMatrix3fv(glGetUniformLocation(ID, name.rawStr()), 1, GL_FALSE, &mat[0][0]);
+    }
+    // ------------------------------------------------------------------------
+    void Shader::setMat4(NOU::NOU_DAT_ALG::StringView8 name, const glm::mat4 &mat) const
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.rawStr()), 1, GL_FALSE, &mat[0][0]);
+    }
 
-    void Shader::setFloat()
-    {}
+    void Shader::checkCompileErrors(GLuint shader, std::string type)
+    {
+        GLint success;
+        GLchar infoLog[1024];
+        if(type != "PROGRAM")
+        {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if(!success)
+            {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+        else
+        {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if(!success)
+            {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
 }
 
