27019ca6cffb39432b40ab07ea8b370dd6c3b85a
Lukas Gross <lukasgross96@web.de>
2018-05-05 17:33:40 +0200

    Implemented the GLFW_CONTEXT_VERSION for Mac.

--- NostraEngine/src/source/nostraengine/window/GLFWWindow.cpp
+++ NostraEngine/src/source/nostraengine/window/GLFWWindow.cpp
@@ -47,6 +47,11 @@ namespace NOE::NOE_WINDOW
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
 		GLFWmonitor* glwfMonitor = reinterpret_cast<GLFWmonitor*>(monitor);
 
 		m_title = title;
