932aa315395479da97b0f105c9f36711de873335
Lukas Reichmann <lukas@reichmann-info.de>
2018-05-08 23:05:33 +0200

    Started converting to the new database layout.

--- NostraEngine/src/header/nostraengine/core/ResourceManagement.hpp
+++ NostraEngine/src/header/nostraengine/core/ResourceManagement.hpp
@@ -14,6 +14,61 @@
 
 namespace NOE::NOE_CORE
 {
+	class NOU_CLASS ResourceType
+	{
+	public:
+		using ID = NOU::int32;
+
+		static constexpr ID INVALID_ID = 0;
+
+		static const NOU::NOU_DAT_ALG::StringView8 SQL_EXISTS_TYPE;
+		
+		static const NOU::NOU_DAT_ALG::StringView8 SQL_GENERIC;
+
+		static const NOU::NOU_DAT_ALG::StringView8 SQL_NAME_NAME;
+
+		static const NOU::NOU_DAT_ALG::StringView8 SQL_DESCRIPTION_NAME;
+
+	private:
+		/**
+		\brief The ID of the type.
+		*/
+		mutable ID m_id; //mutable for isValid()
+
+		/**
+		\param attribute The name of the attribute to get the value from.
+
+		\return The value of the attribute.
+
+		\brief Queries the value of the attribute \p attribute of the type.
+		*/
+		NOU::NOU_DAT_ALG::String8 getAttribute(const NOU::NOU_DAT_ALG::StringView8 &attribute) const;
+
+		NOU::boolean checkIfExsists() const;
+	public:
+		explicit ResourceType(ID id = INVALID_ID);
+
+		ID getID() const;
+
+		NOU::NOU_DAT_ALG::String8 getName() const;
+
+		NOU::NOU_DAT_ALG::String8 getDescription() const;
+
+		/**
+		\return True, if the type is valid, false if not.
+
+		\brief Returns whether the type is valid (it exists in the database) or not.
+		*/
+		NOU::boolean isValid() const;
+
+		/**
+		\return isValid()
+
+		\brief Same as isValid()
+		*/
+		operator NOU::boolean() const;
+	};
+
 	/**
 	\brief A class that can be used to read the meta data of a single resource. 
 	*/
@@ -91,7 +146,7 @@ namespace NOE::NOE_CORE
 		the original ID is created afterwards, the instances of this class that were before that will NOT be
 		updated.
 		*/
-		ResourceMetadata(ID id = INVALID_ID);
+		explicit ResourceMetadata(ID id = INVALID_ID);
 
 		/**
 		\return The ID of the resource.
@@ -693,6 +748,10 @@ namespace NOE::NOE_CORE
 		*/
 		NOE::NOE_UTILITY::sqlite::Database& getUnderlying();
 	};
+
+	constexpr typename ResourceType::ID ResourceType::INVALID_ID;
+
+	constexpr ResourceMetadata::ID ResourceMetadata::INVALID_ID;
 
 	template<typename T, typename... ARGS>
 	Resource* ResourceManager::allocateResource(ARGS&&... args)
--- NostraEngine/src/source/nostraengine/core/ResourceManagement.cpp
+++ NostraEngine/src/source/nostraengine/core/ResourceManagement.cpp
@@ -1,9 +1,127 @@
 #include "nostraengine/core/ResourceManagement.hpp"
 #include "nostraengine/core/ErrorCodes.hpp"
 
+#include "SQLite3/sqlite3.h"
+
+#include <iostream>
+
 namespace NOE::NOE_CORE
 {
-	constexpr ResourceMetadata::ID ResourceMetadata::INVALID_ID;
+	const NOU::NOU_DAT_ALG::StringView8 ResourceType::SQL_EXISTS_TYPE = 
+																"SELECT COUNT(*) FROM Types WHERE ID = ?;";
+
+	const NOU::NOU_DAT_ALG::StringView8 ResourceType::SQL_GENERIC = "SELECT %s FROM Types WHERE ID = ?;";
+
+	const NOU::NOU_DAT_ALG::StringView8 ResourceType::SQL_NAME_NAME = "name";
+
+	const NOU::NOU_DAT_ALG::StringView8 ResourceType::SQL_DESCRIPTION_NAME = "description";
+
+	NOU::NOU_DAT_ALG::String8 ResourceType::getAttribute
+													(const NOU::NOU_DAT_ALG::StringView8 &attribute) const
+	{
+		if (!isValid())
+		{
+			NOU_PUSH_ERROR(NOU::NOU_CORE::getErrorHandler(), 
+				NOU::NOU_CORE::ErrorCodes::INVALID_OBJECT, "A type with that ID does not exist.");
+			return "__INVALID_TYPE_ATTRIBUTE__";
+		}
+
+		NOU::char8 sql[256] = { 0 };
+		sprintf(sql, SQL_GENERIC.rawStr(), attribute.rawStr());
+
+		auto stmt = ResourceManager::get().getUnderlying().execute(sql);
+		stmt.bind(m_id);
+
+		//there should always be a next
+		NOE::NOE_UTILITY::sqlite::Row &row = stmt.next();
+
+		if (!row.isNull(0))
+			return row.valueAs(0, NOE::NOE_UTILITY::sqlite::STRING());
+	}
+
+	NOU::boolean ResourceType::checkIfExsists() const
+	{
+		auto stmt = ResourceManager::get().getUnderlying().execute(SQL_EXISTS_TYPE);
+		stmt.bind(m_id);
+
+		NOE::NOE_UTILITY::sqlite::Row *row;
+
+		if (stmt.hasNext())
+			row = &stmt.next();
+		else
+			return false;
+		
+		if (row->isValid())
+		{
+			NOU::int32 count = row->valueAs(0, NOE::NOE_UTILITY::sqlite::INTEGER());
+
+			if (count == 0) //no type of that ID was found
+			{
+				return false;
+			}
+		}
+		else //the row is not valid
+		{
+			return false;
+		}
+
+		return true;
+	}
+	
+	ResourceType::ResourceType(ID id) :
+		m_id(id)
+	{
+		if (m_id != INVALID_ID) //check if exists, if the id is not invalid anyway
+		{
+			if (!checkIfExsists())
+			{
+				m_id = INVALID_ID;
+			}
+		}
+	}
+
+	typename ResourceType::ID ResourceType::getID() const
+	{
+		return m_id;
+	}
+
+	NOU::NOU_DAT_ALG::String8 ResourceType::getName() const
+	{
+		return getAttribute(SQL_NAME_NAME);
+	}
+
+	NOU::NOU_DAT_ALG::String8 ResourceType::getDescription() const
+	{
+		return getAttribute(SQL_DESCRIPTION_NAME);
+	}
+
+	NOU::boolean ResourceType::isValid() const
+	{
+		if (m_id == INVALID_ID)
+			return false;
+		else
+		{
+			if (checkIfExsists())
+			{
+				//it exists
+				return true;
+			}
+			else
+			{
+				//it does not exist
+				m_id = INVALID_ID;
+				return false;
+			}
+		}
+	}
+
+	ResourceType::operator NOU::boolean() const
+	{
+		return isValid();
+	}
+
+
+
 
 	const NOU::NOU_DAT_ALG::StringView8 ResourceMetadata::SQL_GENERIC = 
 																"SELECT %s FROM Resources WHERE ID = ?;";
@@ -41,7 +159,7 @@ namespace NOE::NOE_CORE
 	{
 		//Check if the ID even exists in the database. If not, set it to INVALID_ID
 
-		if (id != 0)
+		if (id != INVALID_ID)
 		{
 			auto stmt = ResourceManager::get().getUnderlying().execute(SQL_EXISTS_RESOURCE);
 			stmt.bind(id);
