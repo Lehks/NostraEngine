2955fc3827a9d957c09e0d52d66aca0a4ee9eaec
Lukas Reichmann <lukas@reichmann-info.de>
2018-05-28 20:11:53 +0200

    Added a plugin that is used for testing.

---
+++ Plugins/TestPlugin/TestPluginPlugin.cpp
@@ -0,0 +1,28 @@
+#include "TestPluginPlugin.hpp"
+
+namespace TestPluginPlugin
+{
+    NOE::NOE_CORE::Plugin::InitResult TestPluginPlugin::initialize(NOE::NOE_CORE::NostraEngine &engineInstance)
+    {
+		std::cout << "TestPlugin::initialize()" << std::endl;
+
+        return NOE::NOE_CORE::Plugin::InitResult::SUCCESS;
+    }
+
+	NOE::NOE_CORE::Plugin::InitResult TestPluginPlugin::terminate(NOE::NOE_CORE::NostraEngine &engineInstance)
+    {
+		std::cout << "TestPlugin::terminate()" << std::endl;
+
+		return NOE::NOE_CORE::Plugin::InitResult::SUCCESS;
+    }
+        
+	void TestPluginPlugin::receive(NOE::NOE_CORE::Plugin::ID source, void *data, NOU::sizeType size, NOU::uint32 flags)
+    {
+		std::cout << "Received from: " << source << "; Data: " << reinterpret_cast<NOU::char8*>(data) << std::endl;
+    }
+}
+
+NOE_SET_AS_ACTIVE_PLUGIN_CLASS(TestPluginPlugin::TestPluginPlugin);
+
+
+
