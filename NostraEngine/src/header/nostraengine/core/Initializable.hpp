ad016e52a60f2536600c6b22e972421032540e41
NooCi <Dennis2010>
2018-05-29 11:25:40 +0200

    Changed the enum in Initializable from global to class public

--- NostraEngine/src/header/nostraengine/core/Initializable.hpp
+++ NostraEngine/src/header/nostraengine/core/Initializable.hpp
@@ -17,21 +17,20 @@
 */
 namespace NOE::NOE_CORE {
 
-
-	enum class ExitCode
-	{
-		SUCCESS,
-		WARNING,
-		ERROR
-	};
-
-
 	/**
 	\brief This class will be used as interface for other subclasses that need to be initialized before specific tasks can happen.
 		   It also provides a constructor which sets a basic priority for when some objects have to be initialized before others.
 	*/
 	class NOU_CLASS Initializable 
 	{
+		public:
+			enum class ExitCode
+			{
+				SUCCESS,
+				WARNING,
+				ERROR
+			};
+
 		private:
 			/**
 			\brief The priority rating of the current object. 0 is the lowest priority where -1 is the highest
---
+++ NostraEngine/src/header/nostraengine/material_system/ShaderClassInterface.hpp
@@ -0,0 +1,7 @@
+#ifndef NOE_MATSYS_SHADER_CLASS_INTERFACE_HPP
+#define NOE_MATSYS_SHADER_CLASS_INTERFACE_HPP
+
+#include "nostrautils/NostraUtils.hpp"
+#include "nostraengine/NostraEngine.hpp"
+
+#endif
