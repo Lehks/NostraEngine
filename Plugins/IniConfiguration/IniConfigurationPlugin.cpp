7ec06d8338c27f775ef084d63c670bb03814d381
Lukas Reichmann <lukas@reichmann-info.de>
2018-06-14 20:12:32 +0200

    Started implementing the INIConfiguration plugin.

---
+++ Plugins/IniConfiguration/IniConfigurationPlugin.cpp
@@ -0,0 +1,24 @@
+#include "IniConfigurationPlugin.hpp"
+
+namespace IniConfigurationPlugin
+{
+    NOE::NOE_CORE::Plugin::InitResult IniConfigurationPlugin::initialize(NOE::NOE_CORE::NostraEngine &engineInstance)
+    {
+        return NOE::NOE_CORE::Plugin::InitResult::SUCCESS;
+    }
+
+	NOE::NOE_CORE::Plugin::InitResult IniConfigurationPlugin::terminate(NOE::NOE_CORE::NostraEngine &engineInstance)
+    {
+		return NOE::NOE_CORE::Plugin::InitResult::SUCCESS;
+    }
+        
+	void IniConfigurationPlugin::receive(NOE::NOE_CORE::Plugin::ID source, void *data, NOU::sizeType size, NOU::uint32 flags)
+    {
+
+    }
+}
+
+NOE_SET_AS_ACTIVE_PLUGIN_CLASS(IniConfigurationPlugin::IniConfigurationPlugin);
+
+
+
