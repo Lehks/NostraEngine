ae8a065de38ba085195f28157d7d503ed85f17fd
Leslie <leslie@bmarxen.de>
2018-07-10 16:23:58 +0200

    Rewrote internal structure, removed unecessary functions, closes #102

--- NostraEngine/src/header/nostraengine/material_system/AbstractSyntaxTree.hpp
+++ NostraEngine/src/header/nostraengine/material_system/AbstractSyntaxTree.hpp
@@ -5,86 +5,91 @@
 
 namespace NOT
 {
-    class NOU_CLASS ASTNode{
+    class NOU_CLASS AbstractSyntaxTree
+    {
+
+
         public:
+            AbstractSyntaxTree();
+            AbstractSyntaxTree(const AbstractSyntaxTree& other) = delete;
+            AbstractSyntaxTree(AbstractSyntaxTree&& other) = delete;
 
-            friend class ASTNode;
 
-            enum class Types{
-                ENTRY,
-                FUNC_DEC,
-                PARAM_LIST,
-                PARAM,
-                RET_TYPE,
-                TYPE,
-                BODY,
-                VAR_DEC,
-                ASSIGN,
-                VAR,
-                OPERATOR,
-                VAR_CALL,
-                RETURN,
-                CONST_VAL,
-                FUNC_CALL,
-                BREAK,
-                CONTINUE,
-                WHILE,
-                COND,
-                IF,
-                ELSE    
-            };
+            class NOU_CLASS ASTNode{
+                friend AbstractSyntaxTree::AbstractSyntaxTree();
+                friend AbstractSyntaxTree::AbstractSyntaxTree(AbstractSyntaxTree&& other);
+
+            public:
+
+                enum class Types{
+                    ENTRY,
+                    FUNC_DEC,
+                    PARAM_LIST,
+                    PARAM,
+                    RET_TYPE,
+                    TYPE,
+                    BODY,
+                    VAR_DEC,
+                    ASSIGN,
+                    VAR,
+                    OPERATOR,
+                    VAR_CALL,
+                    RETURN,
+                    CONST_VAL,
+                    FUNC_CALL,
+                    BREAK,
+                    CONTINUE,
+                    WHILE,
+                    COND,
+                    IF,
+                    ELSE    
+                };
+
+            private:
+
+                AbstractSyntaxTree* m_assignedTree;
+                NOU::sizeType m_ownIndex;
+                NOU::sizeType m_parent;
+                Types m_type;
+                NOU::NOU_DAT_ALG::String8 m_value;
+                NOU::NOU_DAT_ALG::Vector<NOU::sizeType> m_children;
+
+                ASTNode(Types type, const NOU::NOU_DAT_ALG::String8& value = "");
+            public:
+                Types getType() const;
+                void setType(Types type);
+
+                const NOU::NOU_DAT_ALG::String8& getValue() const;
+                void setValue(const NOU::NOU_DAT_ALG::String8& value);
+                NOU::boolean hasValue() const;
+
+                const ASTNode* const getChild(NOU::sizeType pos) const;
+                ASTNode* getChild(NOU::sizeType pos);
+                NOU::sizeType getChildCount() const;
+                NOU::boolean hasChild() const;
+
+                const ASTNode* const getParent() const;
+                ASTNode* getParent();
+                NOU::boolean hasParent() const;
+
+                NOU::boolean isLeaf() const;
+                NOU::boolean isRoot() const;
+
+                void appendNode(Types type, const NOU::NOU_DAT_ALG::String8& value = "");
+
+                NOU::boolean operator==(const ASTNode& other) const;
+        };
+        friend ASTNode;
 
         private:
-            Types m_type;
-            NOU::NOU_DAT_ALG::String8 m_value;
-            NOU::NOU_DAT_ALG::Vector<ASTNode> m_childs;
-            ASTNode* m_parent;
+            NOU::NOU_DAT_ALG::Vector<NOU::NOU_MEM_MNGT::UniquePtr<ASTNode>>m_childPool;
         public:
-            ASTNode(Types type,const NOU::NOU_DAT_ALG::String8& value = "");
-            ASTNode(ASTNode&& other);
-            // Copy eeds testing if any inconsistent states will occur because of the m_parent pointer.
-            // Needs some Depthsearch deep copy
-            ASTNode(const ASTNode& other) = default;
-            // Hope deleting a whole tree will work
-            ~ASTNode() = default;
 
-            Types getType() const;
-            void setType(Types type);
-
-            const NOU::NOU_DAT_ALG::String8& getValue() const;
-            void setValue(const NOU::NOU_DAT_ALG::String8& value);
-            NOU::boolean hasValue() const;
-
-            const ASTNode* const getChild(NOU::sizeType pos) const;
-            ASTNode* getChild(NOU::sizeType pos);
-            NOU::sizeType getChildCount();
-            NOU::boolean hasChild() const;
-
-            const ASTNode* const getParent() const;
-            ASTNode* getParent();
-            NOU::boolean hasParent() const;
-
-            NOU::boolean isLeaf() const;
-            NOU::boolean isRoot() const;
-
-            // returns true if inserted and false if appended pos >= m_childs.size
-            NOU::boolean insertNode(NOU::sizeType pos, Types type, const NOU::NOU_DAT_ALG::String8& value = "");
-            void appendNode(Types type, const NOU::NOU_DAT_ALG::String8& value = "");
-            NOU::boolean removeNode(NOU::sizeType pos, ASTNode* n = nullptr);
-
-
-            NOU::boolean operator==(const ASTNode& other) const;
-            void operator=(const ASTNode& other);
-
-            ASTNode& operator[](NOU::sizeType i);
-            
-            const ASTNode& operator[](NOU::sizeType i) const;
-
+            ASTNode* getRoot();
+            const ASTNode* const getRoot() const;
     };
 
-    // for easier usage
-    using AbstractSyntaxTree = ASTNode;
-
+    
 }
 
 
--- NostraEngine/src/source/nostraengine/material_system/AbstractSyntaxTree.cpp
+++ NostraEngine/src/source/nostraengine/material_system/AbstractSyntaxTree.cpp
@@ -1,175 +1,150 @@
 #include "nostraengine/material_system/AbstractSyntaxTree.hpp"
-
+#include "nostrautils/NostraUtils.hpp"
+#define NOT_AST AbstractSyntaxTree
+#define NOT_ENTRY NOT_AST::ASTNode::Types::ENTRY
 namespace NOT
 {
-        ASTNode::ASTNode(Types type, const NOU::NOU_DAT_ALG::String8& value) :
-        m_type(type),
-        m_value(value),
-        m_parent(nullptr)
-        { }
+    NOT_AST::AbstractSyntaxTree()
+    {
+        NOU::NOU_MEM_MNGT::UniquePtr<ASTNode> tmp(new ASTNode(NOT_ENTRY), NOU::NOU_MEM_MNGT::defaultDeleter);
+        m_childPool.emplaceBack(NOU::NOU_CORE::move(tmp));
+        m_childPool[0]->m_parent = 0;
+        m_childPool[0]->m_ownIndex = 0;
+        m_childPool[0]->m_assignedTree = this;
+    }
 
-        ASTNode::ASTNode(ASTNode&& other):
-        m_type(NOU::NOU_CORE::move(other.m_type)),
-        m_value(NOU::NOU_CORE::move(other.m_value)),
-        m_parent(other.m_parent),
-        m_childs(NOU::NOU_CORE::move(other.m_childs))
+    // needs debugging
+    /* NOT_AST::AbstractSyntaxTree(AbstractSyntaxTree&& other) :
+    m_childPool(NOU::NOU_CORE::move(other.m_childPool))
+    {
+        NOU::sizeType s = other.m_childPool.size();
+        for(NOU::sizeType i = 0; i < s; i++)
         {
-            other.m_parent = nullptr;
+            m_childPool[i]->m_assignedTree = this;
         }
 
+    }*/
+    NOT_AST::ASTNode* NOT_AST::getRoot()
+    {
+        return m_childPool[0].rawPtr();
+    }
+    const AbstractSyntaxTree::ASTNode* const NOT_AST::getRoot() const
+    {
+        return m_childPool[0].rawPtr();
+    }
+    
+    NOT_AST::ASTNode::ASTNode(NOT_AST::ASTNode::Types type, const NOU::NOU_DAT_ALG::String8& value) :
+    m_type(type),
+    m_value(value)
+    { }
 
+    NOT_AST::ASTNode::Types NOT_AST::AbstractSyntaxTree::ASTNode::getType() const
+    {
+        return m_type;
+    }
 
+    void NOT_AST::ASTNode::setType(NOT_AST::ASTNode::Types type)
+    {
+        m_type = type;
+    }
 
-    	ASTNode::Types ASTNode::getType() const
+    const NOU::NOU_DAT_ALG::String8& NOT_AST::ASTNode::getValue() const
+    {
+        return m_value;
+    }
+
+    NOU::boolean NOT_AST::ASTNode::hasValue() const
+    {
+        return m_value.size() != 0;
+    }
+
+    // on  both following methods add nullptr return
+    const NOT_AST::ASTNode* const NOT_AST::ASTNode::getChild(NOU::sizeType pos) const
+    {
+        if(pos >= m_children.size())
         {
-            return m_type;
+            return nullptr;
         }
+        return m_assignedTree->m_childPool[m_children[pos]].rawPtr();
+    }
 
-        void ASTNode::setType(Types type)
+    NOT_AST::ASTNode* NOT_AST::ASTNode::getChild(NOU::sizeType pos)
+    {
+        if(pos >= m_children.size())
         {
-            m_type = type;
+            return nullptr;
         }
+        return m_assignedTree->m_childPool[m_children[pos]].rawPtr();
+    }
 
+    NOU::sizeType NOT_AST::ASTNode::getChildCount() const
+    {
+        return m_children.size();
+    }
 
-        const NOU::NOU_DAT_ALG::String8& ASTNode::getValue() const
-        {
-            return m_value;
-        }
+    NOU::boolean NOT_AST::ASTNode::hasChild() const
+    {
+        return m_children.size() != 0;
+    }
 
-        void ASTNode::setValue(const NOU::NOU_DAT_ALG::String8& value)
-        {
-            m_value = value;
-        }
+    const NOT_AST::ASTNode* const NOT_AST::ASTNode::getParent() const
+    {
+        return m_assignedTree->m_childPool[m_parent].rawPtr();
+    }
 
-        NOU::boolean ASTNode::hasValue() const
-        {
-            return m_value.size() != 0;
-        }
+    NOT_AST::ASTNode* NOT_AST::ASTNode::getParent()
+    {
+        ASTNode* ret;
+        auto& tmpVec = m_assignedTree->m_childPool;
+        auto& tmpPtr = tmpVec[m_parent];
+        ret = tmpPtr.rawPtr();
+        return ret;
+    }
 
+    NOU::boolean NOT_AST::ASTNode::hasParent() const
+    {
+        return m_ownIndex != m_parent;
+    }
 
-        const ASTNode* const ASTNode::getChild(NOU::sizeType pos) const
-        {
-            if(pos > m_childs.size())
-            {
-                return nullptr;
-            } else
-            {
-                return &m_childs[pos];
-            }
-        }
+    NOU::boolean NOT_AST::ASTNode::isLeaf() const
+    {
+        return !hasChild();
+    }
+
+    NOU::boolean NOT_AST::ASTNode::isRoot() const
+    {
+        return !hasParent();
+    }
+
+    void NOT_AST::ASTNode::appendNode(Types type, const NOU::NOU_DAT_ALG::String8& value)
+    {
+        NOU::NOU_MEM_MNGT::UniquePtr<ASTNode> tmp(new ASTNode(type, value), NOU::NOU_MEM_MNGT::defaultDeleter);
+        NOU::sizeType pos;
+
+        m_assignedTree->m_childPool.emplaceBack(NOU::NOU_CORE::move(tmp));
+        pos = m_assignedTree->m_childPool.size()-1;
         
-        ASTNode* ASTNode::getChild(NOU::sizeType pos)
-        {
-            if(pos > m_childs.size())
-            {
-                return nullptr;
-            } else
-            {
-                return &m_childs[pos];
-            }
-        }
-        
-        NOU::sizeType ASTNode::getChildCount()
-        {
-            return m_childs.size();
-        }
-        
+        //NOU::NOU_MEM_MGNT::UniquePtr<type> ptr(new type(), NOU::NOU_MEM_MGNT::defaultDeleter); 
+        //UniquePtr::rawPtr()
+        //ptr->someMethod()
+        //*ptr
 
-        NOU::boolean ASTNode::hasChild() const
-        {
-            return m_childs.size() != 0;
-        }
+        m_assignedTree->m_childPool[pos]->m_ownIndex = pos;
+        m_assignedTree->m_childPool[pos]->m_parent = m_ownIndex;
+        m_assignedTree->m_childPool[pos]->m_assignedTree = m_assignedTree;
+        m_children.emplaceBack(pos);
+    }
 
+    NOU::boolean NOT_AST::ASTNode::operator==(const NOT_AST::ASTNode& other) const
+    {
+        NOU::boolean b;
 
-        const ASTNode* const ASTNode::getParent() const
-        {
-            return m_parent;
-        }
-        
-        ASTNode* ASTNode::getParent()
-        {
-            return m_parent;
-        }
-        
-        NOU::boolean ASTNode::hasParent() const
-        {
-            return m_parent != nullptr;
-        }
+        b = m_type == other.m_type;
+        b &= m_value == other.m_value;
 
-        NOU::boolean ASTNode::isLeaf() const
-        {
-            return hasChild();
-        }
-
-        NOU::boolean ASTNode::isRoot() const
-        {
-            return hasParent();
-        }
-
-        NOU::boolean ASTNode::insertNode(NOU::sizeType pos, Types type, const NOU::NOU_DAT_ALG::String8& value)
-        {
-            ASTNode tmp(type, value);
-            tmp.m_parent = this;
-            if(pos >= m_childs.size())
-            {
-                m_childs.emplaceBack(NOU::NOU_CORE::move(tmp));
-                return false;
-            } else
-            {
-                m_childs.insert(pos, NOU::NOU_CORE::move(tmp));
-                return true;
-            }
-        }
-
-
-        void ASTNode::appendNode(Types type, const NOU::NOU_DAT_ALG::String8& value)
-        {
-            ASTNode tmp(type, value);
-            tmp.m_parent = this;
-
-            m_childs.emplaceBack(NOU::NOU_CORE::move(tmp));
-        }
-        
-        NOU::boolean ASTNode::removeNode(NOU::sizeType pos, ASTNode* n)
-        {
-            if(pos < m_childs.size())
-            {
-                if(n != nullptr)
-                {
-                    *n = NOU::NOU_CORE::move(m_childs[pos]);
-                }
-                m_childs.remove(pos);
-                return true;
-            }
-            return false;
-        }
-
-        NOU::boolean ASTNode::operator==(const ASTNode& other) const
-        {
-            NOU::boolean r;
-            r = m_type == other.m_type;
-            r &= m_value == other.m_value;
-            return r;
-        }
-
-        void ASTNode::operator=(const ASTNode& other)
-        {
-            m_type = other.m_type;
-            m_value = other.m_value;
-            m_childs = other.m_childs;
-        }
-
-
-        ASTNode& ASTNode::operator[](NOU::sizeType i)
-        {
-            return *getChild(i);
-        }
-
-        const ASTNode& ASTNode::operator[](NOU::sizeType i) const
-        {
-            return *getChild(i);
-        }
-        
-        
+        return b;
+    }
 
 }
+
+#undef NOT_AST
