9f679b62b14c35d9ee2f765008bbc405c88aaf89
Leslie <leslie@bmarxen.de>
2018-07-24 12:52:27 +0200

    Temporary disabled huge amounts of Compile errors

--- Plugins/GLSLGenerator/src/generator/GLSLGenerator.cpp
+++ Plugins/GLSLGenerator/src/generator/GLSLGenerator.cpp
@@ -1,7 +1,7 @@
-// #include "GLSLGenerator.hpp"
+#include "GLSLGenerator.hpp"
 
-// namespace GLSLGeneratorPlugin
-// {
+namespace GLSLGeneratorPlugin
+{
 
         NOU::sizeType GLSLGenerator::processState(const NOT::AbstractSyntaxTree::ASTNode::Types &STATE)
         {
@@ -111,109 +111,113 @@
             return 1;
         }
 
+        /* Each of the following one liners was missing a return statement -> compile error */
+
         NOU::sizeType  GLSLGenerator::bodyState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::retTypeState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::varState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::arrDefState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::assignState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::breakState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::condState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::constValueState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::continueState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::elseState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::entryState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::externState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::forState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::funcCallState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::funcDecState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::idState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::ifState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::operatorState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::paramState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::paramListState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::returnState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::runVarState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::sizeState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::structDefState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::varCallState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::whileState()
-        {}
+        { return 0; }
 
         NOU::sizeType  GLSLGenerator::structCallState()
-        {}
+        { return 0; }
 
-//         GLSLGenerator::GLSLGenerator(NOT::AbstractSyntaxTree &ast) :
-//         m_outputSourceFile(nullptr),
-//         m_outputSourceString("")
-//         {
-//             m_ast = NOU::NOU_CORE::move(ast);
+        GLSLGenerator::GLSLGenerator(NOT::AbstractSyntaxTree &ast) :
+        m_outputSourceFile(nullptr),
+        m_outputSourceString("")
+        {
+            // m_ast = NOU::NOU_CORE::move(ast); /* AST has no operator=, you can initialize with m_ast(NOU::NOU_CORE::move(ast)) */
 
-//         }
+        }
 
-//         NOU::boolean GLSLGenerator::start(const NOU::NOU_DAT_ALG::Vector<NOU::NOU_DAT_ALG::String8> &paramArgs)
-//         {
-//             if(paramArgs.size() == 0)
-//             {
-//                 m_root = m_ast.getRoot();
-//                 m_outputSourceFile = NOU::NOU_FILE_MNGT::File(NOU::NOU_FILE_MNGT::Path::currentWorkingDirectory().getAbsolutePath());
-//                 processTree(m_root);
-//             }else if (paramArgs.size() == 1)
-//             {
-//                 m_root = m_ast.getRoot();
-//                 m_outputSourceFile = NOU::NOU_FILE_MNGT::File(paramArgs.at(0));
-//                 processTree(m_root);
-//             }
-//         }
+        NOU::boolean GLSLGenerator::start(const NOU::NOU_DAT_ALG::Vector<NOU::NOU_DAT_ALG::String8> &paramArgs)
+        {
+            if(paramArgs.size() == 0)
+            {
+                m_root = m_ast.getRoot();
+                // m_outputSourceFile = NOU::NOU_FILE_MNGT::File(NOU::NOU_FILE_MNGT::Path::currentWorkingDirectory().getAbsolutePath()); /* File cannot be reasigned, use a newly created File object or store the actual Path as attribute */
+                processTree(m_root);
+            }else if (paramArgs.size() == 1)
+            {
+                m_root = m_ast.getRoot();
+                // m_outputSourceFile = NOU::NOU_FILE_MNGT::File(paramArgs.at(0)); /* File cannot be reasigned, use a newly created File object or store the actual Path as attribute */
+                processTree(m_root);
+            }
+
+            return false; /* don't forget to return a value */
+        }
 
         NOU::boolean GLSLGenerator::processTree(NOT::AbstractSyntaxTree::ASTNode *nextNode)
         {
@@ -242,12 +246,12 @@
                 }
             }
 
-//             return true;
-//         }
+            return true;
+        }
 
-//         NOU::boolean GLSLGenerator::generateSource()
-//         {
-//             return true;
-//         }
+        NOU::boolean GLSLGenerator::generateSource()
+        {
+            return true;
+        }
 
-// }
+}
--- Plugins/GLSLGenerator/src/generator/GLSLGenerator.hpp
+++ Plugins/GLSLGenerator/src/generator/GLSLGenerator.hpp
@@ -1,9 +1,9 @@
-// #ifndef PLUGIN_GLSL_GENERATOR_HPP
-// #define PLUGIN_GLSL_GENERATOR_HPP
+#ifndef PLUGIN_GLSL_GENERATOR_HPP
+#define PLUGIN_GLSL_GENERATOR_HPP
 
-// #include "nostrautils/NostraUtils.hpp"
-// #include "nostraengine/NostraEngine.hpp"
-// #include "nostraengine/material_system/Generator.hpp"
+#include "nostrautils/NostraUtils.hpp"
+#include "nostraengine/NostraEngine.hpp"
+#include "nostraengine/material_system/Generator.hpp"
 
 // /**
 // \file generator/GLSLGenerator.hpp
@@ -14,8 +14,8 @@
 
 // \brief A Generator that generate GLSL code from a given AST.
 // */
-// namespace GLSLGeneratorPlugin
-// {
+namespace GLSLGeneratorPlugin
+{
 
     class GLSLGenerator : public NOE::NOE_MATSYS::Generator
     {
@@ -27,42 +27,42 @@
         NOU::NOU_DAT_ALG::String8           m_outputSourceString;
         NOE_PLUGIN_FUNC NOU::sizeType processState(const NOT::AbstractSyntaxTree::ASTNode::Types & STATE) override;
 
-//         NOE_PLUGIN_FUNC NOU::sizeType  varDecState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  bodyState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  retTypeState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  varState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  arrDefState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  assignState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  breakState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  condState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  constValueState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  continueState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  elseState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  entryState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  externState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  forState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  funcCallState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  funcDecState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  idState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  ifState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  operatorState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  paramState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  paramListState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  returnState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  runVarState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  sizeState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  structDefState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  varCallState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  whileState();
-//         NOE_PLUGIN_FUNC NOU::sizeType  structCallState();
+        NOE_PLUGIN_FUNC NOU::sizeType  varDecState();
+        NOE_PLUGIN_FUNC NOU::sizeType  bodyState();
+        NOE_PLUGIN_FUNC NOU::sizeType  retTypeState();
+        NOE_PLUGIN_FUNC NOU::sizeType  varState();
+        NOE_PLUGIN_FUNC NOU::sizeType  arrDefState();
+        NOE_PLUGIN_FUNC NOU::sizeType  assignState();
+        NOE_PLUGIN_FUNC NOU::sizeType  breakState();
+        NOE_PLUGIN_FUNC NOU::sizeType  condState();
+        NOE_PLUGIN_FUNC NOU::sizeType  constValueState();
+        NOE_PLUGIN_FUNC NOU::sizeType  continueState();
+        NOE_PLUGIN_FUNC NOU::sizeType  elseState();
+        NOE_PLUGIN_FUNC NOU::sizeType  entryState();
+        NOE_PLUGIN_FUNC NOU::sizeType  externState();
+        NOE_PLUGIN_FUNC NOU::sizeType  forState();
+        NOE_PLUGIN_FUNC NOU::sizeType  funcCallState();
+        NOE_PLUGIN_FUNC NOU::sizeType  funcDecState();
+        NOE_PLUGIN_FUNC NOU::sizeType  idState();
+        NOE_PLUGIN_FUNC NOU::sizeType  ifState();
+        NOE_PLUGIN_FUNC NOU::sizeType  operatorState();
+        NOE_PLUGIN_FUNC NOU::sizeType  paramState();
+        NOE_PLUGIN_FUNC NOU::sizeType  paramListState();
+        NOE_PLUGIN_FUNC NOU::sizeType  returnState();
+        NOE_PLUGIN_FUNC NOU::sizeType  runVarState();
+        NOE_PLUGIN_FUNC NOU::sizeType  sizeState();
+        NOE_PLUGIN_FUNC NOU::sizeType  structDefState();
+        NOE_PLUGIN_FUNC NOU::sizeType  varCallState();
+        NOE_PLUGIN_FUNC NOU::sizeType  whileState();
+        NOE_PLUGIN_FUNC NOU::sizeType  structCallState();
 
-//     public:
-//         NOE_PLUGIN_FUNC GLSLGenerator(NOT::AbstractSyntaxTree &ast);
-//         NOE_PLUGIN_FUNC NOU::boolean start(const NOU::NOU_DAT_ALG::Vector<NOU::NOU_DAT_ALG::String8> &paramArgs) override;
-//         NOE_PLUGIN_FUNC NOU::boolean processTree(NOT::AbstractSyntaxTree::ASTNode *m_ast) override;
-//         NOE_PLUGIN_FUNC NOU::boolean generateSource() override;
+    public:
+        NOE_PLUGIN_FUNC GLSLGenerator(NOT::AbstractSyntaxTree &ast);
+        NOE_PLUGIN_FUNC NOU::boolean start(const NOU::NOU_DAT_ALG::Vector<NOU::NOU_DAT_ALG::String8> &paramArgs) override;
+        NOE_PLUGIN_FUNC NOU::boolean processTree(NOT::AbstractSyntaxTree::ASTNode *m_ast) override;
+        NOE_PLUGIN_FUNC NOU::boolean generateSource() override;
 
-//     };
+    };
 
-// }
-// #endif
+}
+#endif
