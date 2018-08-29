b60ae287aa39cdb19039b5430947e5ad57cccf07
Dennis Franz <dennisfranz96@gmx.net>
2018-08-29 21:09:12 +0200

    further implemented material interface

--- NostraEngine/src/header/nostraengine/material_system/Material.hpp
+++ NostraEngine/src/header/nostraengine/material_system/Material.hpp
@@ -22,11 +22,11 @@ namespace NOE::NOE_MATSYS
 		Program *m_shader;
 		Texture *m_texture;
 
-		NOU::NOU_MATH::Color m_color;
-		NOU::NOU_MATH::Color m_ambientColor;
-		NOU::NOU_MATH::Color m_diffuseColor;
-		NOU::NOU_MATH::Color m_specularColor;
-		NOU::NOU_MATH::Color m_emissionColor;
+		NOU::NOU_MATH::Color32f m_color;
+		NOU::NOU_MATH::Color32f m_ambientColor;
+		NOU::NOU_MATH::Color32f m_diffuseColor;
+		NOU::NOU_MATH::Color32f m_specularColor;
+		NOU::NOU_MATH::Color32f m_emissionColor;
 
 		NOU::boolean m_parrallaxMappingisActive;
 		NOU::boolean m_normalMappingisActive;
@@ -35,10 +35,10 @@ namespace NOE::NOE_MATSYS
 		NOU::boolean createShader();
 		NOU::boolean createTexture();
 	public:
-		Material(const NOU_DAT_ALG::String8 &materialName);
-		Material(const NOU_DAT_ALG::String8 &materialName, const Program &shader);
-		Material(const NOU_DAT_ALG::String8 &materialName, const Program &shader, const Texture &texture);
-		Material(const NOU_DAT_ALG::String8 &materialName, const Program &shader, const Texture &texture, const NOU::NOU_MATH::Color &color);
+		Material(const NOU::NOU_DAT_ALG::String8 &materialName);
+		Material(const NOU::NOU_DAT_ALG::String8 &materialName, const Program &shader);
+		Material(const NOU::NOU_DAT_ALG::String8 &materialName, const Program &shader, const Texture &texture);
+		Material(const NOU::NOU_DAT_ALG::String8 &materialName, const Program &shader, const Texture &texture, const NOU::NOU_MATH::Color32f &color);
 
 		NOU::boolean bindShader();
 		NOU::boolean unbindShader();
@@ -65,25 +65,25 @@ namespace NOE::NOE_MATSYS
 		void setShader(const NOE::NOE_MATSYS::Program &shader);
 		void setTexture(const NOE::NOE_MATSYS::Texture &texture);
 
-		void setColor(const NOU::NOU_MATH::Color &color);
-		void setColor(NOU::float32 r, NOU::float32 g, NOU::float32 b, NOU::float32 a = 1.0f);
+		void setColor(const NOU::NOU_MATH::Color32f &color);
+		void setColor(NOU::float32 r, NOU::float32 g, NOU::float32 b, NOU::byte a = 255);
 
-		void setAmbientColor(const NOU::NOU_MATH::Color &color);
-		void setAmbientColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::float32 a = 1.0f);
-		void setDiffuseColor(const NOU::NOU_MATH::Color &color);
-		void setDiffuseColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::float32 a = 1.0f);
-		void setSpecularColor(const NOU::NOU_MATH::Color &color);
-		void setSpecularColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::float32 a = 1.0f);
-		void setEmissionColor(const NOU::NOU_MATH::Color &color);
-		void setEmissionColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::float32 a = 1.0f);
+		void setAmbientColor(const NOU::NOU_MATH::Color32f &color);
+		void setAmbientColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::byte a = 255);
+		void setDiffuseColor(const NOU::NOU_MATH::Color32f &color);
+		void setDiffuseColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::byte a = 255);
+		void setSpecularColor(const NOU::NOU_MATH::Color32f &color);
+		void setSpecularColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::byte a = 255);
+		void setEmissionColor(const NOU::NOU_MATH::Color32f &color);
+		void setEmissionColor(NOU::byte r, NOU::byte g, NOU::byte b, NOU::byte a = 255);
 
 		NOU::NOU_DAT_ALG::String8 getMaterialName();
 
-		NOU::NOU_MATH::Color getColor();
-		NOU::NOU_MATH::Color getAmbientColor();
-		NOU::NOU_MATH::Color getDiffuseColor();
-		NOU::NOU_MATH::Color getSpecularColor();
-		NOU::NOU_MATH::Color getEmissionColor();
-	}
+		NOU::NOU_MATH::Color32f getColor();
+		NOU::NOU_MATH::Color32f getAmbientColor();
+		NOU::NOU_MATH::Color32f getDiffuseColor();
+		NOU::NOU_MATH::Color32f getSpecularColor();
+		NOU::NOU_MATH::Color32f getEmissionColor();
+	};
 }
 #endif
